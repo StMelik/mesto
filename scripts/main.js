5671e9fe94c83a27618fb3a7c03ab11e81dcec7e
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-01-10 19:27:23 +0300

    feat: добавить popup логику

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+let profileInfo = document.querySelector('.profile__info');
+let popup = document.querySelector('.popup');
+
+let formElement = popup.querySelector('.popup__form');
+
+let profileName = profileInfo.querySelector('.profile__name');
+let popupName = popup.querySelector('.popup__name');
+
+let profileAbout = profileInfo.querySelector('.profile__about');
+let popupAbout = popup.querySelector('.popup__about');
+
+let openPopup = document.querySelector('.profile__edit-button');
+let closePopup = popup.querySelector('.popup__close');
+
+
+popupName.value = profileName.textContent
+popupAbout.value = profileAbout.textContent
+
+
+openPopup.addEventListener('click', popupOpend)
+closePopup.addEventListener('click', popupOpend)
+
+function popupOpend() {
+    popup.classList.toggle('popup_opend')
+}
+
+// ----------------------------------
+
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    let namePopupValue = popupName.value;
+    let aboutPopupValue = popupAbout.value;
+
+    profileName.textContent = namePopupValue;
+    profileAbout.textContent = aboutPopupValue;
+
+    popupOpend()
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 

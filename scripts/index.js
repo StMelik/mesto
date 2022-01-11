4c2d86cbcf48e6bddc549b22a1c9cd8b2503585d
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-01-12 00:50:12 +0300

    fix: исправить работу попапа

--- scripts/index.js
+++ scripts/index.js
@@ -12,18 +12,15 @@ let popupAbout = popup.querySelector('.popup__about');
 let openPopup = document.querySelector('.profile__edit-button');
 let closePopup = popup.querySelector('.popup__close');
 
-
-openPopup.addEventListener('click', popupOpend)
-closePopup.addEventListener('click', popupOpend)
-
 function popupOpend() {
     popup.classList.toggle('popup_opend');
     popupName.value = profileName.textContent
     popupAbout.value = profileAbout.textContent
 }
 
-// ----------------------------------
-
+function popupClosed() {
+    popup.classList.toggle('popup_opend');
+}
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -37,4 +34,6 @@ function formSubmitHandler(evt) {
     popupOpend()
 }
 
+openPopup.addEventListener('click', popupOpend)
+closePopup.addEventListener('click', popupClosed)
 formElement.addEventListener('submit', formSubmitHandler); 

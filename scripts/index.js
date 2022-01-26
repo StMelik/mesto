99b645fe262039284c631cb00f31583ff5442923
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-01-26 19:03:11 +0300

    feat: добавить отрисовку карточек через JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    // {
+    //     name: 'Байкал',
+    //     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    // }
+];
+
 let profileInfo = document.querySelector('.profile__info');
 let popup = document.querySelector('.popup');
 
@@ -11,6 +38,19 @@ let popupAbout = popup.querySelector('.popup__input_value_about');
 
 let openPopup = document.querySelector('.profile__edit-button');
 let closePopup = popup.querySelector('.popup__close');
+
+const cardTemp = document.querySelector('#card').content;
+const elements = document.querySelector('.elements');
+
+initialCards.forEach(function (item) {
+    const cardElement = cardTemp.querySelector('.element').cloneNode(true);
+
+    cardElement.querySelector('.element__image').src = item.link;
+    cardElement.querySelector('.element__image').alt = item.name;
+    cardElement.querySelector('.element__title').textContent = item.name;
+
+    elements.append(cardElement);
+})
 
 function popupOpend() {
     popup.classList.toggle('popup_opend');

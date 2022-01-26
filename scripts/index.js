76a8a4639e8491cd5a204dd03062da01c57fafbd
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-01-26 21:11:04 +0300

    feat: добавить логику добавления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -26,24 +26,24 @@ const initialCards = [
 ];
 
 const profileInfo = document.querySelector('.profile__info');
-const popupEdit = document.querySelector('.popup_edit');
-
-const formElementEdit = popupEdit.querySelector('.popup__form');
-
 const profileName = profileInfo.querySelector('.profile__name');
-const popupName = popupEdit.querySelector('.popup__input_value_name');
-
 const profileAbout = profileInfo.querySelector('.profile__about');
-const popupAbout = popupEdit.querySelector('.popup__input_value_about');
 
+// Попап редактирования профиля
+const popupEdit = document.querySelector('.popup_edit');
 const openPopupEdit = document.querySelector('.profile__edit-button');
 const closePopupEdit = popupEdit.querySelector('.popup__close');
+const formElementEdit = popupEdit.querySelector('.popup__form');
+const popupEditName = popupEdit.querySelector('.popup__input_value_name');
+const popupEditAbout = popupEdit.querySelector('.popup__input_value_about');
 
 // Попап добавления карточки
 const popupAdd = document.querySelector('.popup_add');
 const openPopupAdd = document.querySelector('.profile__add-button');
 const closePopupAdd = popupAdd.querySelector('.popup__close');
 const formElementAdd = popupAdd.querySelector('.popup__form');
+const popupAddName = popupAdd.querySelector('.popup__input_value_name');
+const popupAddLink = popupAdd.querySelector('.popup__input_value_link');
 
 const cardTemp = document.querySelector('#card').content;
 const elements = document.querySelector('.elements');
@@ -57,36 +57,62 @@ initialCards.forEach(function (item) {
     elements.append(cardElement);
 })
 
+// Открытие попапа
 function popupOpend(popup) {
     popup.classList.toggle('popup_opend');
     if (popup === popupEdit) {
-        popupName.value = profileName.textContent
-        popupAbout.value = profileAbout.textContent
+        popupEditName.value = profileName.textContent
+        popupEditAbout.value = profileAbout.textContent
     }
 }
 
+// Закрытие попапа
 function popupClosed(popup) {
     popup.classList.toggle('popup_opend');
+}
+
+// Добавить новую карточку
+function addCard() {
+    const cardElement = cardTemp.querySelector('.element').cloneNode(true);
+    cardElement.querySelector('.element__image').src = initialCards[initialCards.length - 1].link;
+    cardElement.querySelector('.element__image').alt = initialCards[initialCards.length - 1].name;
+    cardElement.querySelector('.element__title').textContent = initialCards[initialCards.length - 1].name;
+    elements.prepend(cardElement);
 }
 
 function formSubmitHandlerPopupEdit(evt) {
     evt.preventDefault();
 
-    let namePopupValue = popupName.value;
-    let aboutPopupValue = popupAbout.value;
-
+    let namePopupValue = popupEditName.value;
+    let aboutPopupValue = popupEditAbout.value;
     profileName.textContent = namePopupValue;
     profileAbout.textContent = aboutPopupValue;
 
     popupClosed(popupEdit)
 }
 
+function formSubmitHandlerPopupAdd(evt) {
+    evt.preventDefault();
 
+    let newCard = {
+        name: popupAddName.value,
+        link: popupAddLink.value
+    }
+    initialCards.push(newCard)
 
+    addCard()
+    popupClosed(popupAdd)
+
+    popupAddName.value = '';
+    popupAddLink.value = '';
+}
+
+// Обрботка событий попапа редактирования профиля
 openPopupEdit.addEventListener('click', () => popupOpend(popupEdit))
 closePopupEdit.addEventListener('click', () => popupOpend(popupEdit))
 formElementEdit.addEventListener('submit', formSubmitHandlerPopupEdit);
 
+// Обрботка событий попапа добавления карточек
 openPopupAdd.addEventListener('click', () => popupOpend(popupAdd))
 closePopupAdd.addEventListener('click', () => popupOpend(popupAdd))
 formElementAdd.addEventListener('submit', formSubmitHandlerPopupAdd);

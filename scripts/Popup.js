a61c91e8ef2130f7e775c9b7b55f25805efc3d15
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-03-15 14:00:00 +0300

    refactor: переписать на класс управление попапом

---
+++ scripts/Popup.js
@@ -0,0 +1,40 @@
+export default class Popup {
+    constructor(popupSelector) {
+        this._popup = document.querySelector(popupSelector)
+    }
+
+    _handleEscClose(evt) {
+        const isKeyEscape = evt.key === 'Escape'
+        if (isKeyEscape) {
+            this.close()
+        }
+    }
+
+    _handleClickClose(evt) {
+        const isOverlay = evt.target.classList.contains('popup');
+        const isButtonClose = evt.target.classList.contains('popup__close');
+        if (isOverlay || isButtonClose) {
+            this.close();
+        }
+    }
+
+    _removeEventListeners() {
+        this._popup.removeEventListener('mousedown', this._handleClickClose);
+        document.removeEventListener('keydown', this._handleEscClose);
+    }
+
+    setEventListeners() {
+        this._popup.addEventListener('mousedown', this._handleClickClose);
+        document.addEventListener('keydown', this._handleEscClose);
+    }
+
+    open() {
+        this._popup.classList.add('popup_opened');
+        this.setEventListeners()
+    }
+
+    close() {
+        this._popup.classList.remove('popup_opened');
+        this._removeEventListeners()
+    }
+}
